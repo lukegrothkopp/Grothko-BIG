5976b4f4931812d98fc5b5950bd70309a2d2db27
lukegrothkopp <lukegrothkopp@gmail.com>
2025-10-23 09:37:51 -0700

    Delete tests directory

--- tests/test_basic.py
+++
@@ -1,6 +0,0 @@
-from src.data_loader import load_dataframe
-
-def test_load():
-    df = load_dataframe('data/sample_sales.csv')
-    assert len(df) > 0
-    assert set(['date','region','product','customer_id','age','gender','quantity','sales','discount_pct']).issubset(df.columns)
