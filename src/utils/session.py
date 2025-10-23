da0bef53741ba76814490fea5a7eee249336ddc9
lukegrothkopp <lukegrothkopp@gmail.com>
2025-10-23 09:37:11 -0700

    Delete src/utils directory

--- src/utils/session.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def init_state():
-    st.session_state.setdefault("chat_history", [])
-    st.session_state.setdefault("data_loaded", False)
-    st.session_state.setdefault("df", None)
-    st.session_state.setdefault("rag_index", None)
-    st.session_state.setdefault("rag_texts", [])
-    st.session_state.setdefault("data_summary", "")
